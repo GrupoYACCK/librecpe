2aa9073ed34bd7d14fe8e5fec23d21cf35639238
Alex Cuellar <acuellar@grupoyacck.com>
2026-06-02 14:24:01 -0500

    [FIX] actualiza versión a 3.1.3 en setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PACKAGE_DIR = 'src'
 
 setup(
     name=PACKAGE_NAME,
-    version='3.1.2',
+    version='3.1.3',
     author='Alex Cuellar',
     author_email='acuellar@grupoyacck.com',
     maintainer='Alex Cuellar',
